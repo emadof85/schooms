561e7c695ad74c65a2a7ca0f561c5980174640b6
Emad Kh <ekarhilli@gmail.com>
2025-09-28 20:43:17 +0300

    modified:   public/assets/css/qs.css 	modified:   resources/views/pages/support_team/students/edit.blade.php 	modified:   resources/views/pages/support_team/users/edit.blade.php 	modified:   resources/views/pages/support_team/users/index.blade.php

--- resources/views/pages/support_team/users/edit.blade.php
+++ resources/views/pages/support_team/users/edit.blade.php
@@ -149,3 +149,108 @@
 
     </div>
 @endsection
+
+@section('scripts')
+    	<script>
+    		// --- Translations for jQuery Steps ---
+            @php
+                $wizardTranslations = [
+                    'previous' => __('msg.previous'),
+                    'next' => __('msg.next'),
+                    'finish' => __('msg.finish'),
+                ];
+            @endphp
+
+            const wizardLabels = {!! json_encode($wizardTranslations, JSON_UNESCAPED_UNICODE) !!};
+            
+            //
+            // Wizard with validation
+            //
+    
+            // Show form
+            var form = $('.steps-validation').show();
+            
+            // Initialize wizard
+            $('.steps-validation').steps({
+                headerTag: 'h6',
+                bodyTag: 'fieldset',
+                titleTemplate: '<span class="number">#index#</span> #title#',
+                labels: {
+                    previous: '<i class="icon-arrow-{{app()->getLocale() == 'ar' ? 'right14': 'left13'}} mr-2" /> '+ wizardLabels.previous,
+                    next: wizardLabels.next +' <i class="icon-arrow-{{app()->getLocale() == 'ar' ? 'left13': 'right14'}} ml-2" />',
+                    finish: wizardLabels.finish +' <i class="icon-arrow-{{app()->getLocale() == 'ar' ? 'left13': 'right14'}} ml-2" />'
+                },
+                transitionEffect: 'fade',
+                autoFocus: true,
+                onStepChanging: function (event, currentIndex, newIndex) {
+    
+                    // Always allow previous action even if the current form is not valid!
+                    if (currentIndex > newIndex) {
+                        return true;
+                    }
+    
+                    // Needed in some cases if the user went back (clean up)
+                    if (currentIndex < newIndex) {
+    
+                        // To remove error styles
+                        form.find('.body:eq(' + newIndex + ') label.error').remove();
+                        form.find('.body:eq(' + newIndex + ') .error').removeClass('error');
+                    }
+    
+                    form.validate().settings.ignore = ':disabled,:hidden';
+                    return form.valid();
+                },
+                onFinishing: function (event, currentIndex) {
+                    form.validate().settings.ignore = ':disabled';
+                    return form.valid();
+                },
+                onFinished: function (event, currentIndex) {
+                    $(this).submit();
+                }
+            });
+    
+    
+            // Initialize validation
+            $('.steps-validation').validate({
+                ignore: 'input[type=hidden], .select2-search__field', // ignore hidden fields
+                errorClass: 'validation-invalid-label',
+                highlight: function(element, errorClass) {
+                    $(element).removeClass(errorClass);
+                },
+                unhighlight: function(element, errorClass) {
+                    $(element).removeClass(errorClass);
+                },
+    
+                // Different components require proper error label placement
+                errorPlacement: function(error, element) {
+    
+                    // Unstyled checkboxes, radios
+                    if (element.parents().hasClass('form-check')) {
+                        error.appendTo( element.parents('.form-check').parent() );
+                    }
+    
+                    // Input with icons and Select2
+                    else if (element.parents().hasClass('form-group-feedback') || element.hasClass('select2-hidden-accessible')) {
+                        error.appendTo( element.parent() );
+                    }
+    
+                    // Input group, styled file input
+                    else if (element.parent().is('.uniform-uploader, .uniform-select') || element.parents().hasClass('input-group')) {
+                        error.appendTo( element.parent().parent() );
+                    }
+    
+                    // Other elements
+                    else {
+                        error.insertAfter(element);
+                    }
+                },
+                rules: {
+                    email: {
+                        email: true
+                    }
+                }
+            });
+
+        </script>
+        
+    @endsection
